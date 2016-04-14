b96010b6819e3012622b9bd9b5959ad75b268af3
rafaellucio <rafaelantoniolucio@gmail.com>
2016-04-14 19:08:30 -0300

    changing code to new sintax ES6

--- app/App.js
+++ app/App.js
@@ -1,8 +1,7 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
-var Router = require('react-router').Router;
-var routes = require('./config/routes');
-var hashHistory = require('react-router').hashHistory;
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Router, hashHistory } from 'react-router';
+import routes from './config/routes';
 
 ReactDOM.render(
 	<Router history={hashHistory}>{routes}</Router>,
